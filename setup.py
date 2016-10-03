afe42c12918916d8c635d697ba257a751f5437a8
Darryl Lane <darryllane101@gmail.com>
2016-10-03 22:15:48 +0100

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
     packages=['Bluto'],
-    py_modules = ['/modules/'],
+    py_modules = ['Bluto/modules/__init__.py'],
     include_package_data=True,
     license='LICENSE.txt',
     description='''
