2dfc01710db98bff68a9a102f36744b60d951fd1
Darryl Lane <darryllane101@gmail.com>
2017-01-13 01:00:17 +0000

    Version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='2.3.8',
+    version='2.3.9',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
@@ -23,7 +23,7 @@ setup(
         "pythonwhois",
         "lxml",
         "oletools",
-        "pdfminer",
+        "pdfminer"
     ],
 )
 
