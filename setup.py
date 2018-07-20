a4515e8b062834f9c89a03827e342548781d3c02
Darryl Lane <darryllane@hp2515g1.home>
2018-07-21 00:00:34 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='2.4.11',
+    version='2.4.12',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
