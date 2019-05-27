bee7eadfe744e4eed102374bb4ea2e7fe284e8c5
Darryl Lane <darryllane101@gmail.com>
2019-05-27 23:07:02 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='3.0b',
+    version='3.0.1b',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
@@ -13,7 +13,7 @@ setup(
     DNS Recon | Brute Forcer | DNS Zone Transfer | DNS Wild Card Checks
     DNS Wild Card Brute Forcer | Email Enumeration | Staff Enumeration
     Compromised Account Checking''',
-    long_description=open('README.md').read(),
+    long_description_content_type=open('README.md').read(),
     scripts=['Bluto/bluto'],
     install_requires=[
         "bs4",
