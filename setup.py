91497247ba653387396faa5b721139807789ac21
Darryl Lane <darryllane101@gmail.com>
2019-05-27 23:00:54 +0100

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Encryption',
-    version='0.1b.0dev',
+    name='Bluto',
+    version='3.0b',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
-    packages=['Encryption'],
+    url='https://github.com/darryllane/Bluto',
+    packages=['Bluto'],
     include_package_data=True,
     license='LICENSE.txt',
     description='''
-    Create encrypted data in a file, and test decrypt.
-    ''',
+    DNS Recon | Brute Forcer | DNS Zone Transfer | DNS Wild Card Checks
+    DNS Wild Card Brute Forcer | Email Enumeration | Staff Enumeration
+    Compromised Account Checking''',
     long_description=open('README.md').read(),
-    
-    scripts=['risk_user_check.py'],
+    scripts=['Bluto/bluto'],
     install_requires=[
-        "Fernet",
-        "argparse",
+        "bs4",
+		"certifi",
+		"chardet",
+		"EasyProcess",
+		"idna",
+		"lxml",
+		"progressbar",
+		"pythonwhois",
+		"PyVirtualDisplay",
+		"requests",
+		"selenium",
+		"tqdm",
+		"urllib3",
+		"dnspython",
+		"termcolor",
+		"BeautifulSoup4",
+		"requests[security]",
+		"oletools",
+		"pdfminer"
     ],
 )
 
