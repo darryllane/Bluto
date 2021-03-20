11eb2c78fa979cb902336a6ad5ac6e7dd197351c
Aashik Samuel <aashiksamuel@users.noreply.github.com>
2021-03-20 19:48:48 +0530

    Simple Edit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='2.4.17',
+    version='3',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/darryllane/Bluto',
